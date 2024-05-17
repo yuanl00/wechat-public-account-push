98771f80ea74c7a95951a577be218fafb861ae42
Hu_y <273637057@qq.com>
2024-05-17 11:18:16 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -31,12 +31,32 @@ const USER_CONFIG = {
 		}],
 		"customizedDateList": []
 	}],
+	TIAN_API: {
+	    // 天行API KEY，如果使用天行API则需要填写此项
+	    key: '9723b13e791c15b44313964c7dc557bd',
+	    
+	    /** 天行API相关，需要config中配置 TIAN_API_KEY  */
+	    // 早安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+	    morningGreeting: true,
+	  
+	    // 晚安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+	    eveningGreeting: false,
+	  
+	    // 天行天气（展示未来N天，最多7天）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
+	    weather: 3,
+	  
+	    // 全网热搜榜（展示N条，最多30条）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
+	    networkHot: 3,
+	  
+	    // 全网热搜榜展示类型，默认展示概要信息: ['title': 仅展示标题, 'default': 展示概要信息]
+	    networkHotType: 'default',
+	  },
 	"SWITCH": {
 		"weather": true,
 		"holidaytts": true,
-		"CIBA": true,
+		"CIBA": false,
 		"oneTalk": false,
-		"earthyLoveWords": false,
+		"earthyLoveWords": true,
 		"momentCopyrighting": false,
 		"poisonChickenSoup": false,
 		"poetry": false,
