45b5f3237611c0f1c22bb19dfddcf86a24d2cce6
Hu_y <273637057@qq.com>
2024-05-17 15:16:08 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -2,7 +2,7 @@
 const TEMPLATE_CONFIG = [
   {
     id: '0001',
-    title: '❤️亲爱的, 早上好',
+    title: 'Hey, baby',
     desc: `
       **{{date.DATA}}**
       
@@ -24,7 +24,7 @@ const TEMPLATE_CONFIG = [
       
       ---
       
-      今天是我们相识的第{{love_day.DATA}}天
+      ❤️今天是我们相识的第{{love_day.DATA}}天
       
       {{birthday_message.DATA}}
       
