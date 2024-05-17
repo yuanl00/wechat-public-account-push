197c69c50744f8d2820ab16057cbcde58272bcf3
Hu_y <273637057@qq.com>
2024-05-17 13:44:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -17,7 +17,7 @@ const USER_CONFIG = {
 	"USERS": [{
 		"name": "test",
 		"id": "oxXP96ZS3wGhJokGIi9WjF_cOsG0",
-		"useTemplateId": "cfJIvFMOxNHYYWS4rBkAj71azDpQO3v66VOnnMZpvsc",
+		"useTemplateId": "G6xn9WX3hnaGh4_yvpzU-NvVDka5CvN4dx0Ote6AvRw",
 		"province": "浙江",
 		"city": "绍兴",
 		"horoscopeDate": "05-02",
